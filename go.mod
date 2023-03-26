ccb3b51cd7a8b919fe0745c2a06a0ed1b64cf779
eveperc <eve.perc@icloud.com>
2023-03-26 13:14:08 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,24 @@
+module github.com/eveperc/gopherjs-example
+
+go 1.19
+
+require (
+	github.com/bep/gr v0.0.0-20170817090531-52c9d7f2f11a // indirect
+	github.com/divan/three v0.0.0-20190518222012-e7fb2420829c // indirect
+	github.com/fatih/structs v1.1.0 // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/gmlewis/go-babylonjs v0.0.1 // indirect
+	github.com/gopherjs/gopherjs v1.17.2 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/neelance/astrewrite v0.0.0-20160511093645-99348263ae86 // indirect
+	github.com/neelance/sourcemap v0.0.0-20200213170602-2833bce08e4c // indirect
+	github.com/shurcooL/httpfs v0.0.0-20190707220628-8d4bc4ba7749 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
+	github.com/spf13/cobra v1.6.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/crypto v0.7.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/term v0.6.0 // indirect
+	golang.org/x/tools v0.7.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+    "github.com/divan/three"
+    "github.com/gopherjs/gopherjs/js"
+)
+
+func main() {
+    width := js.Global.Get("innerWidth").Float()
+    height := js.Global.Get("innerHeight").Float()
+
+    renderer := three.NewWebGLRenderer()
+    renderer.SetSize(width, height, true)
+    js.Global.Get("document").Get("body").Call("appendChild", renderer.Get("domElement"))
+
+    camera := three.NewPerspectiveCamera(70, width/height, 1, 1000)
+    camera.Position.Set(0, 0, 500)
+
+    scene := three.NewScene()
+
+    light := three.NewDirectionalLight(three.NewColor("white"), 1)
+    light.Position.Set(0, 256, 256)
+    scene.Add(light)
+
+    params := three.NewMaterialParameters()
+    params.Color = three.NewColor("blue")
+    mat := three.NewMeshLambertMaterial(params)
+
+    geom := three.NewBoxGeometry(&three.BoxGeometryParameters{
+        Width:  200,
+        Height: 200,
+        Depth:  200,
+    })
+    mesh := three.NewMesh(geom, mat)
+    scene.Add(mesh)
+
+    var animate func()
+    animate = func() {
+        js.Global.Call("requestAnimationFrame", animate)
+        mesh.Rotation.Set("y", mesh.Rotation.Get("y").Float()+0.01)
+        renderer.Render(scene, camera)
+    }
+    animate()
+}
